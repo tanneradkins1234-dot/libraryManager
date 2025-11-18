eb98decae63ebffe699490c72832c019decb5dea
tanneradkins1234 <tanneradkins1234@gmail.com>
2025-11-18 18:57:46 -0500

    Need To Fix Json Appendage

---
+++ main.c
@@ -0,0 +1,176 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <cjson/cJSON.h>
+
+int addBooks() {
+	char author [50];
+	char bookName[100];
+	
+	cJSON *json = cJSON_CreateObject();
+	getchar();
+	printf("Input The Author: \n");
+	fgets(author, 50, stdin);
+	author[strcspn(author, "\n")] = 0;
+	printf("Author Entered: %s\n", author);
+	printf("|--------------------------|\n");
+	printf("Adding String To JSON object...\n");	
+	cJSON_AddStringToObject(json, "author", author);
+	printf("String Added\n");
+	printf("|---------------------------|\n");
+
+	printf("Input The Book Name: ");
+	fgets(bookName, 100, stdin);
+	bookName[strcspn(bookName, "\n")] = 0;
+	printf("Book Name Entered: %s\n", bookName);
+	printf("|--------------------------|\n");
+ 	printf("Adding String To JSON object...\n");
+	cJSON_AddStringToObject(json, "bookName", bookName);
+	printf("String Added\n");
+	printf("|--------------------------|\n");
+	printf("Converting JSON object to a JSON string...\n");
+	char *json_str = cJSON_Print(json);
+	printf("Conversion Complete\n");
+	printf("|--------------------------|\n");
+	printf("Writing JSON string to file: books.json...\n");
+	FILE *fp = fopen("books.json", "a+");
+	if (fp == NULL) {
+		printf("Error: Unable to open the file.\n");
+		return 1;
+	}
+	fseek(fp, 0, SEEK_END);
+	printf("%s\n", json_str);
+	fputs(json_str, fp);
+	fclose;
+	printf("JSON String Succesully Written\n");
+	cJSON_free(json_str);
+	cJSON_Delete(json);
+	printf("Book Added Successfully \n");
+
+	return 0;
+}
+
+int removeBooks() {
+	printf("This Is Where You Remove Books\n");
+	return 0;
+}
+
+int viewBooks() {
+	printf("This is Where You View Books\n");
+	return 0;
+}
+
+int addMembers() {
+	char name[50];
+	char email[100];
+	char phoneNumber[50];
+
+	cJSON *json = cJSON_CreateObject();
+	getchar();
+	printf("Input Member Name: ");
+	fgets(name, 50, stdin);
+	name[strcspn(name, "\n")] = 0;
+	printf("Name Entered: %s\n", name);
+	printf("|--------------------------|\n");
+	printf("Adding String To JSON Object...\n");
+	cJSON_AddStringToObject(json, "name", name);
+	printf("String Added\n");
+	printf("|--------------------------|\n");
+	
+	printf("Input Member Email: ");
+	fgets(email, 100, stdin);
+	email[strcspn(email, "\n")] = 0;
+	printf("Email Entered: %s\n", email);
+	printf("|--------------------------|\n");
+	printf("Adding String To JSON Object...\n");
+	cJSON_AddStringToObject(json, "email", email);
+	printf("String Added\n");
+	printf("|--------------------------|\n");
+
+	printf("Input Member Phone Number: ");
+	fgets(phoneNumber, 50, stdin);
+	phoneNumber[strcspn(phoneNumber, "\n")] = 0;
+	printf("Phone Number Entered: \n");
+	printf("|--------------------------|\n");
+	printf("Adding String To JSON Object...");
+	cJSON_AddStringToObject(json, "phoneNumber", phoneNumber);
+	printf("String Added\n");
+	printf("|--------------------------|\n");
+
+	printf("Converting JSON object to a JSON string...\n");
+	char *json_str = cJSON_Print(json);
+	printf("Conversion Complete\n");
+	printf("|--------------------------|\n");
+	printf("Writing JSON string to file: members.json...\n");
+	FILE *fp = fopen("members.json", "a+");
+	if (fp == NULL) {
+		printf("Error: Unable to open the file.\n");
+		return 1;
+	}
+	fseek(fp, 0, SEEK_END);
+	printf("%s\n", json_str);
+	fputs(json_str, fp);
+	fclose;
+	printf("JSON String Succesully Written\n");
+	cJSON_free(json_str);
+	cJSON_Delete(json);
+	printf("Member Added Successfully \n");
+
+	return 0;
+}
+
+int editMRecords() {
+	printf("This Is Where You Edit Member Records\n");
+	return 0;
+}
+
+int viewMembers() {
+	printf("This Is Where You View Members\n");
+	return 0;
+}
+
+
+
+int main() {
+	printf("|----------Library Manager---------|\n");
+	printf("|-----Enter a Number-----|\n");
+	printf("1. Add a Book\n");
+	printf("2. Remove a Book\n");
+	printf("3. View Books\n");
+	printf("4. Add a Member\n");
+	printf("5. Edit Member Records\n");
+	printf("6. View Members\n");
+	printf("7. Quit\n");
+	printf("|-----Enter a Number-----|\n");
+	
+	int usrInp;
+	scanf("%d", &usrInp);
+
+	if (usrInp == 1) {
+		addBooks();
+	}
+	else if (usrInp == 2) {	
+		removeBooks();
+	}
+	else if (usrInp == 3) {
+		viewBooks();
+	}
+	else if (usrInp == 4) {
+		addMembers();		
+	}
+	else if (usrInp == 5) {
+		editMRecords();
+	}
+	else if (usrInp == 6) {
+		viewMembers();
+	}
+	else if (usrInp == 7) {
+		exit(0);
+	}
+	else {
+		printf("Error: Invalid Input\n");
+		printf("Please Input a Number (1-7)\n");
+	}
+	
+	return 0;
+}	
